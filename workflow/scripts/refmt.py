f28bbaefdd107c4725f8becbcb3b1542ce74f5f4
mrvollger <mrvollger@gmail.com>
2021-06-12 09:49:54 -0700

    lots of formatting

--- workflow/scripts/refmt.py
+++ workflow/scripts/refmt.py
@@ -7,48 +7,68 @@ import argparse
 import pandas as pd
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser = argparse.ArgumentParser(
+        description="", formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
     parser.add_argument("infile", help="samid table")
     parser.add_argument("outfile", help="output bed file")
     parser.add_argument("--window", help="window size", type=int, required=True)
-    parser.add_argument("--full",
-                        help="place to drop the full bed file with the extra columns",
-                        default=None)
-    parser.add_argument("--ncolors", help="number of color qauntiles", type=int, default=10)
+    parser.add_argument(
+        "--full",
+        help="place to drop the full bed file with the extra columns",
+        default=None,
+    )
+    parser.add_argument(
+        "--ncolors", help="number of color qauntiles", type=int, default=10
+    )
     parser.add_argument("--fai", help="fai for the genome", required=True)
-    parser.add_argument("-d", help="store args.d as true if -d",  action="store_true", default=False)
+    parser.add_argument(
+        "-d", help="store args.d as true if -d", action="store_true", default=False
+    )
     args = parser.parse_args()
-
 
     df = pd.read_csv(args.infile, sep="\t")
     # only keep the alignment with the most matching bases
-    df.sort_values(by=["query_name",
-                       "reference_name",
-                       "matches"], inplace=True, ascending=False)
+    df.sort_values(
+        by=["query_name", "reference_name", "matches"], inplace=True, ascending=False
+    )
     df.drop_duplicates(subset=["query_name", "reference_name"], inplace=True)
     # extract the position from the name and add it to the start and end
-    q_fix = df.query_name.str.extract(r'(.+):(\d+)-\d+', expand=True)
+    q_fix = df.query_name.str.extract(r"(.+):(\d+)-\d+", expand=True)
     df.query_name = q_fix[0]
-    df.query_start =  q_fix[1].astype(int)
+    df.query_start = q_fix[1].astype(int)
     df.query_end = q_fix[1].astype(int) + args.window
     # same for ref coords
-    r_fix = df.reference_name.str.extract(r'(.+):(\d+)-\d+', expand=True)
+    r_fix = df.reference_name.str.extract(r"(.+):(\d+)-\d+", expand=True)
     df.reference_name = r_fix[0]
     df.reference_start = r_fix[1].astype(int)
     df.reference_end = r_fix[1].astype(int) + args.window
-    
-    #limit the size if we go over the chr end
-    fai = pd.read_csv(args.fai, sep="\t",names=["chr", "length", "x","y","z"])[["chr","length"]]
+
+    # limit the size if we go over the chr end
+    fai = pd.read_csv(args.fai, sep="\t", names=["chr", "length", "x", "y", "z"])[
+        ["chr", "length"]
+    ]
     df = pd.merge(df, fai, left_on="query_name", right_on="chr")
     df = pd.merge(df, fai, left_on="reference_name", right_on="chr")
-    df.loc[df.query_end >= df.length_x, "length_x"] = df.loc[df.query_end >= df.length_x, "length_x"] - 1 
-    df.loc[df.reference_end >= df.length_y, "length_y"] = df.loc[df.reference_end >= df.length_y, "length_y"] - 1 
-    
+    df.loc[df.query_end >= df.length_x, "length_x"] = (
+        df.loc[df.query_end >= df.length_x, "length_x"] - 1
+    )
+    df.loc[df.reference_end >= df.length_y, "length_y"] = (
+        df.loc[df.reference_end >= df.length_y, "length_y"] - 1
+    )
+
     # columns to keep in the defualt output
-    keep_cols = ["query_name","query_start","query_end",
-                 "reference_name","reference_start",
-                 "reference_end","perID_by_events", "strand"]
-    out=df
+    keep_cols = [
+        "query_name",
+        "query_start",
+        "query_end",
+        "reference_name",
+        "reference_start",
+        "reference_end",
+        "perID_by_events",
+        "strand",
+    ]
+    out = df
     out2 = out.copy()
     out2.reference_name = out.query_name
     out2.reference_start = out.query_start
@@ -57,39 +77,42 @@ if __name__ == "__main__":
     out2.query_start = out.reference_start
     out2.query_end = out.reference_end
 
-    out=pd.concat([out,
-                   out2]
-                  ).sort_values(by=["query_name",
-                                    "query_start",
-                                    "reference_name",
-                                    "reference_start", 
-                                    'perID_by_events'], ascending = False
-                                ).drop_duplicates(
-                                    subset=["query_name", 
-                                            'query_start',
-                                            "reference_name",
-                                            'reference_start']
-                                ).sort_values(by=["query_name",
-                                                  "query_start",
-                                                  "reference_name",
-                                                  "reference_start", 
-                                                  "query_end"
-                                                  ]
-                                )
+    out = (
+        pd.concat([out, out2])
+        .sort_values(
+            by=[
+                "query_name",
+                "query_start",
+                "reference_name",
+                "reference_start",
+                "perID_by_events",
+            ],
+            ascending=False,
+        )
+        .drop_duplicates(
+            subset=["query_name", "query_start", "reference_name", "reference_start"]
+        )
+        .sort_values(
+            by=[
+                "query_name",
+                "query_start",
+                "reference_name",
+                "reference_start",
+                "query_end",
+            ]
+        )
+    )
 
-    out["qcut"] = pd.qcut(out["perID_by_events"],
-                          args.ncolors, 
-                          duplicates="drop",
-                          labels=False)
-   
-    #sys.stdout.write("#"+"\t".join(keep_cols)+"\n")
+    out["qcut"] = pd.qcut(
+        out["perID_by_events"], args.ncolors, duplicates="drop", labels=False
+    )
+
+    # sys.stdout.write("#"+"\t".join(keep_cols)+"\n")
     to_out = out[keep_cols]
     out_header = keep_cols
     out_header[0] = "#" + out_header[0]
     to_out.to_csv(args.outfile, index=False, header=out_header, sep="\t")
-    
+
     if args.full is not None:
         out.to_csv(args.full, index=False, sep="\t")
-
-
 
--- workflow/scripts/samIdentity.py
+++ workflow/scripts/samIdentity.py
@@ -2,39 +2,50 @@
 import argparse
 
 parser = argparse.ArgumentParser(description="")
-parser.add_argument("samfile", help="input sam or bam file" )
-#parser.add_argument("",help="",default=None)
-parser.add_argument('-d', action="store_true", default=False)
-parser.add_argument('-m', '--matches', 
-                    help='min num of matches to return', type=int, default=0)
+parser.add_argument("samfile", help="input sam or bam file")
+# parser.add_argument("",help="",default=None)
+parser.add_argument("-d", action="store_true", default=False)
+parser.add_argument(
+    "-m", "--matches", help="min num of matches to return", type=int, default=0
+)
 parser.add_argument("--header", action="store_true", default=False)
 args = parser.parse_args()
-DEBUG=args.d
+DEBUG = args.d
 
 import glob
 import os
 import sys
 import re
-import pysam 
+import pysam
 
 
 def makeHeader():
-	rtn = ""
-	rtn += "{}\t{}\t{}\t{}\t".format("query_name", "query_start", "query_end", "query_length" )
-	rtn += "{}\t{}\t{}\t".format("reference_name", "reference_start", "reference_end" )
-	rtn += "{}\t{}\t{}\t".format("perID_by_matches", "perID_by_events", "perID_by_all")
-	rtn += "{}\t{}\t{}\t{}\t{}\t{}\t{}".format("matches", "mismatches", 
-			"insertions", "deletions", "insertion_events", "deletion_events", "strand")
-	return(rtn)
+    rtn = ""
+    rtn += "{}\t{}\t{}\t{}\t".format(
+        "query_name", "query_start", "query_end", "query_length"
+    )
+    rtn += "{}\t{}\t{}\t".format("reference_name", "reference_start", "reference_end")
+    rtn += "{}\t{}\t{}\t".format("perID_by_matches", "perID_by_events", "perID_by_all")
+    rtn += "{}\t{}\t{}\t{}\t{}\t{}\t{}".format(
+        "matches",
+        "mismatches",
+        "insertions",
+        "deletions",
+        "insertion_events",
+        "deletion_events",
+        "strand",
+    )
+    return rtn
+
 
 def perId(matches, mismatch, ins, dele, insEvent, delEvent):
-	if( (matches + mismatch) == 0):
-		return((0,0,0))
+    if (matches + mismatch) == 0:
+        return (0, 0, 0)
 
-	bymatches = (100.0 * matches)/(matches + mismatch)
-	byevents = (100.0 * matches)/(matches + mismatch + insEvent + delEvent)
-	byall = (100.0 * matches)/(matches + mismatch + ins + dele)
-	return((bymatches, byevents, byall))
+    bymatches = (100.0 * matches) / (matches + mismatch)
+    byevents = (100.0 * matches) / (matches + mismatch + insEvent + delEvent)
+    byall = (100.0 * matches) / (matches + mismatch + ins + dele)
+    return (bymatches, byevents, byall)
 
 
 # /(:[0-9]+|\*[a-z][a-z]|[=\+\-][A-Za-z]+)+/
@@ -44,49 +55,56 @@ def perId(matches, mismatch, ins, dele, insEvent, delEvent):
 # *at indicates reference base a is substituted with a query base t.
 # It is similar to the MD SAM tag but is standalone and easier to parse.
 
+
 def formatRead(read):
     rtn = ""
-    rtn += "{}\t{}\t{}\t{}\t".format(read.query_name,
-                                     read.query_alignment_start,
-                                     read.query_alignment_end,
-                                     read.infer_query_length() )
-    rtn += "{}\t{}\t{}\t".format(read.reference_name,
-                                 read.reference_start,
-                                 read.reference_end )
+    rtn += "{}\t{}\t{}\t{}\t".format(
+        read.query_name,
+        read.query_alignment_start,
+        read.query_alignment_end,
+        read.infer_query_length(),
+    )
+    rtn += "{}\t{}\t{}\t".format(
+        read.reference_name, read.reference_start, read.reference_end
+    )
     if read.flag == 4:
-        return ''
-    
+        return ""
+
     tags = read.get_tags()
-    
-    if 'cs' in tags:
-        cs = read.get_tag('cs')
+
+    if "cs" in tags:
+        cs = read.get_tag("cs")
         pattern = ":[0-9]+"
         match = re.findall(pattern, cs)
         if match is not None:
-            match = [ int(x[1:]) for x in match ]
+            match = [int(x[1:]) for x in match]
             matches = sum(match)
-        
+
         pattern = "-[a-z]+"
         match = re.findall(pattern, cs)
         if match is not None:
-            match = [ len((x[1:])) for x in match ]
+            match = [len((x[1:])) for x in match]
             delEvent = len(match)
             dele = sum(match)
 
         pattern = "\+[a-z]+"
         match = re.findall(pattern, cs)
         if match is not None:
-            match = [ len((x[1:])) for x in match ]
+            match = [len((x[1:])) for x in match]
             insEvent = len(match)
             ins = sum(match)
 
         mismatch = cs.count("*")
     else:
         counts, events = read.get_cigar_stats()
-        if 'MD' in tags:
-            mismatch = sum([ bp.islower()
-                            for _r, _q, bp in read.get_aligned_pairs(with_seq=True)
-                            if bp is not None])
+        if "MD" in tags:
+            mismatch = sum(
+                [
+                    bp.islower()
+                    for _r, _q, bp in read.get_aligned_pairs(with_seq=True)
+                    if bp is not None
+                ]
+            )
             matches = counts[0] - mismatch
         else:
             matches = counts[7]
@@ -95,31 +113,18 @@ def formatRead(read):
         dele = counts[2]
         insEvent = events[1]
         delEvent = events[2]
-    
+
     strand = "+"
     if read.is_reverse:
-        strand="-"
-    bymatches, byevents, byall = perId(matches,
-                                       mismatch,
-                                       ins,
-                                       dele,
-                                       insEvent,
-                                       delEvent)
-    rtn += "{}\t{}\t{}\t".format(bymatches,
-                                 byevents,
-                                 byall )
-    rtn += "{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(matches,
-                                                 mismatch,
-                                                 ins,
-                                                 dele,
-                                                 insEvent,
-                                                 delEvent,
-                                                 strand)
+        strand = "-"
+    bymatches, byevents, byall = perId(matches, mismatch, ins, dele, insEvent, delEvent)
+    rtn += "{}\t{}\t{}\t".format(bymatches, byevents, byall)
+    rtn += "{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(
+        matches, mismatch, ins, dele, insEvent, delEvent, strand
+    )
     if matches >= args.matches:
         return rtn
-    return ''
-
-
+    return ""
 
 
 if args.header:
@@ -128,8 +133,4 @@ samfile = pysam.AlignmentFile(args.samfile, threads=8)
 for read in samfile.fetch(until_eof=True):
     sys.stdout.write(formatRead(read))
 samfile.close()
-
-
-
-
 
