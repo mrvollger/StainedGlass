1b9dc6f7eeea54d82cfcfa3115c73542acd26f25
Mitchell Robert Vollger <mrvollger@gmail.com>
2021-08-02 14:06:18 -0700

    adding one-to-one mode

--- workflow/scripts/refmt.py
+++ workflow/scripts/refmt.py
@@ -6,6 +6,7 @@ import sys
 import argparse
 import pandas as pd
 
+
 # from dask.distributed import Client, progress
 # import dask
 # import dask.dataframe as dd
@@ -43,28 +44,25 @@ if __name__ == "__main__":
     # df = dd.read_csv(args.infile, sep="\t")
     df = pd.read_csv(args.infile, sep="\t")
 
-    # determine duplicates by these columns
-    det_dup_cols = ["query_name", "reference_name"]
-    if args.one:
-        det_dup_cols = ["reference_name"]
-    # only keep the alignment with the most matching bases
-    df.sort_values(by=det_dup_cols + ["matches"], inplace=True, ascending=False)
-    df.drop_duplicates(subset=det_dup_cols, inplace=True)
+    if not args.one:
+        # determine duplicates by these columns
+        det_dup_cols = ["query_name", "reference_name"]
+        # only keep the alignment with the most matching bases
+        df.sort_values(by=det_dup_cols + ["matches"], inplace=True, ascending=False)
+        df.drop_duplicates(subset=det_dup_cols, inplace=True)
 
     # extract the position from the name and add it to the start and end
     q_fix = df.query_name.str.extract(r"(.+):(\d+)-\d+", expand=True)
     df.query_name = q_fix[0]
     df.query_start = q_fix[1].astype(int)
     df.query_end = q_fix[1].astype(int) + args.window
-    # same for ref coords
-    r_fix = df.reference_name.str.extract(r"(.+):(\d+)-\d+", expand=True)
-    df.reference_name = r_fix[0]
-    df.reference_start = r_fix[1].astype(int)
-    df.reference_end = r_fix[1].astype(int) + args.window
 
-    # remove same reference comparisons in 1-1 mode
-    if args.one:
-        df = df[df.reference_name != df.query_name]
+    if not args.one:
+        # extract ref coords
+        r_fix = df.reference_name.str.extract(r"(.+):(\d+)-\d+", expand=True)
+        df.reference_name = r_fix[0]
+        df.reference_start = r_fix[1].astype(int)
+        df.reference_end = r_fix[1].astype(int) + args.window
 
     # limit the size if we go over the chr end
     fai = pd.read_csv(args.fai, sep="\t", names=["chr", "length", "x", "y", "z"])[
@@ -91,42 +89,51 @@ if __name__ == "__main__":
         "strand",
     ]
     out = df
-    out2 = out.copy()
-    out2.reference_name = out.query_name
-    out2.reference_start = out.query_start
-    out2.reference_end = out.query_end
-    out2.query_name = out.reference_name
-    out2.query_start = out.reference_start
-    out2.query_end = out.reference_end
+    if not args.one:
+        out2 = out.copy()
+        out2.reference_name = out.query_name
+        out2.reference_start = out.query_start
+        out2.reference_end = out.query_end
+        out2.query_name = out.reference_name
+        out2.query_start = out.reference_start
+        out2.query_end = out.reference_end
 
-    out = (
-        pd.concat([out, out2])
-        .sort_values(
-            by=[
-                "query_name",
-                "query_start",
-                "reference_name",
-                "reference_start",
-                "perID_by_events",
-            ],
-            ascending=False,
+        out = (
+            pd.concat([out, out2])
+            .sort_values(
+                by=[
+                    "query_name",
+                    "query_start",
+                    "reference_name",
+                    "reference_start",
+                    "perID_by_events",
+                ],
+                ascending=False,
+            )
+            .drop_duplicates(
+                subset=[
+                    "query_name",
+                    "query_start",
+                    "reference_name",
+                    "reference_start",
+                ]
+            )
+            .sort_values(
+                by=[
+                    "query_name",
+                    "query_start",
+                    "reference_name",
+                    "reference_start",
+                    "query_end",
+                ]
+            )
         )
-        .drop_duplicates(
-            subset=["query_name", "query_start", "reference_name", "reference_start"]
-        )
-        .sort_values(
-            by=[
-                "query_name",
-                "query_start",
-                "reference_name",
-                "reference_start",
-                "query_end",
-            ]
-        )
-    )
 
     out["qcut"] = pd.qcut(
-        out["perID_by_events"], args.ncolors, duplicates="drop", labels=False
+        out["perID_by_events"],
+        min(args.ncolors, out.shape[0]),
+        duplicates="drop",
+        labels=False,
     )
 
     # sys.stdout.write("#"+"\t".join(keep_cols)+"\n")
